012700bcd0f82189a8411e1c1ffb8c26a8f1e40f
David Irvine <david.irvine@maidsafe.net>
2015-02-25 01:41:00 +0000

    Rename to SelfEncryptor

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,50 @@
 extern crate rand;
 extern crate crypto;
-use encryption::{ };
 use std::collections::HashMap;
 use std::cmp;
+use std::old_io::TempDir;
 // this is pub to test the tests dir integration tests these are temp and need to be
 // replaced with actual integration tests and this should be private
 pub mod encryption;
 
 struct DataMap;
 
-pub struct SelfEncryption {
+pub struct SelfEncryptor {
   /* this_data_map: DataMap, */
   /* sequencer: Vec<u8>, */
   /* chunks: HashMap::new(), */
   file_size: u64,
   closed: bool,
+  /* tempdir: TempDir, */
   }
 
-impl SelfEncryption {
+/* impl Default for SelfEncryptor { */
+/*   fn default() -> SelfEncryptor { */
+/*   SelfEncryptor {file_size: 0, closed: false, tempdir: CreateTempDir()} */
+/*     } */
+/*   } */
+
+impl SelfEncryptor {
+  
   pub fn write(&mut self, data: &str ,length: u32, position: u64) {
-    self.file_size = cmp::max(self.file_size, length as u64 + position);
+    let new_size = cmp::max(self.file_size, length as u64 + position);
+    /* self.Preparewindow(length, position, true); */
+    /* for i in 0u64..length as u64 { */
+    /*   self.sequencer[position + i] = data[i] as u8; */
+    /*   } */
+    /*   */
+    self.file_size = new_size;
     }
   
+  fn Preparewindow(&mut self, length: u32, position: u64, write: bool) {
+    }
   
-  
-  
+  fn CreateTempDir() ->TempDir {
+    match TempDir::new("self_encryptor") {
+      Ok(dir) => dir,
+        Err(e) => panic!("couldn't create temporary directory: {}", e)
+    }
+    }
   
   }
 
@@ -35,7 +55,7 @@ impl SelfEncryption {
 
 #[test]
 fn check_write() {
-  let mut se = SelfEncryption{file_size: 0u64, closed: false};
+  let mut se = SelfEncryptor{file_size: 0u64, closed: false};
   se.write("dsd", 3u32, 5u64);
   assert_eq!(se.file_size, 8u64);
 }
