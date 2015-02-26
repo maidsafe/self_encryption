c497e0da0a755f2704752d69ddd4d083920a0d12
David Irvine <david.irvine@maidsafe.net>
2015-02-26 10:05:52 +0000

    DataMap helper methods

--- src/datamap.rs
+++ src/datamap.rs
@@ -18,9 +18,25 @@ impl DataMap {
   fn len(&self)->u64 {
     let size = 0u64;
       match *self {
-       DataMap::Chunks(ref chunks) => 0u64, 
+       DataMap::Chunks(ref chunks) => DataMap::ChunksLength(chunks), 
        DataMap::Content(ref content) => content.len() as u64, 
         }
     }
+  fn has_chunks(&self)->bool {
+      match *self {
+       DataMap::Chunks(ref chunks) => true, 
+       _ => false, 
+        }
+    }
+    fn ChunksLength(chunks: &Vec<ChunkDetails>)->u64 {
+        let mut size = 0u64;
+        for i in chunks.iter() {
+            size += i.source_size
+          }
+          return size
+      }
+      fn empty(&self)->bool {
+         self.len() == 0u64
+        }
   }
 
