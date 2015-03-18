3cd83f5fd8e58efe8e7dc21d1f4f4f756fa90155
Kevin Butler <haqkrs@gmail.com>
2015-03-18 20:43:31 +0000

    Remove EOL spacing

--- src/datamap.rs
+++ src/datamap.rs
@@ -1,6 +1,6 @@
 
 /// Struct holds pre and post encryption hashes as well as original chunk size
-#[derive(PartialEq, Eq, PartialOrd, Ord)] 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub struct ChunkDetails {
   /// index number (starts at 0)
   pub chunk_num: u32,
@@ -8,7 +8,7 @@ pub struct ChunkDetails {
   pub hash: Vec<u8>,
   /// Pre encryption hash of chunk
   pub pre_hash: Vec<u8>,
-  /// size before encryption (compression alters this as well as any poss padding 
+  /// size before encryption (compression alters this as well as any poss padding
   /// depending on cipher used)
   pub source_size: u64
 }
@@ -32,12 +32,12 @@ impl Clone for ChunkDetails {
 
 /// Holds the infomation that required to recover the content of the encrypted file
 /// Depends on the file size, such info can be held as vector of ChunkDetail, or as raw data directly
-#[derive(PartialEq, Eq, PartialOrd, Ord)] 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub enum DataMap {
-  /// if file was large enough (larger than 3072 bytes, 3 * MIN_CHUNK_SIZE) 
+  /// if file was large enough (larger than 3072 bytes, 3 * MIN_CHUNK_SIZE)
   /// this holds the list of chunks' info to decrypt it
   Chunks(Vec<ChunkDetails>),
-  /// very small files (less than 3072 bytes, 3 * MIN_CHUNK_SIZE) are put here in entirely 
+  /// very small files (less than 3072 bytes, 3 * MIN_CHUNK_SIZE) are put here in entirely
   Content(Vec<u8>),
   /// empty datamap
   None
@@ -48,8 +48,8 @@ impl DataMap {
   pub fn len(&self)->u64 {
     let size = 0u64;
     match *self {
-      DataMap::Chunks(ref chunks) => DataMap::chunks_size(chunks), 
-        DataMap::Content(ref content) => content.len() as u64, 
+      DataMap::Chunks(ref chunks) => DataMap::chunks_size(chunks),
+        DataMap::Content(ref content) => content.len() as u64,
         DataMap::None => 0u64
     }
   }
@@ -61,7 +61,7 @@ impl DataMap {
     }
   }
 
-  /// we require this to be a sorted list to allow get_pad_iv_key to get the correct 
+  /// we require this to be a sorted list to allow get_pad_iv_key to get the correct
   /// pre encryption hashes for decrypt/encrypt
   pub fn get_sorted_chunks(&self)->Vec<ChunkDetails> {
     match *self {
@@ -69,7 +69,7 @@ impl DataMap {
                                         let mut result = chunks.to_vec();
                                         DataMap::chunks_sort(result.as_mut_slice());
                                         result
-                                      }, 
+                                      },
         _                           => panic!("no chunks")
     }
   }
@@ -77,8 +77,8 @@ impl DataMap {
   /// content stored as chunks or as raw
   pub fn has_chunks(&self)->bool {
     match *self {
-      DataMap::Chunks(ref chunks) => DataMap::chunks_size(chunks) > 0, 
-        _ => false, 
+      DataMap::Chunks(ref chunks) => DataMap::chunks_size(chunks) > 0,
+        _ => false,
     }
   }
 
