5241dfa36ff25b51defed53aa6b27e71502ed596
Benjamin Bollen <benjamin.bollen@maidsafe.net>
2015-04-01 01:35:19 +0100

    Simplification of example; progress!

--- examples/self_encryption/main.rs
+++ examples/self_encryption/main.rs
@@ -23,7 +23,7 @@ extern crate self_encryption;
 
 use std::path;
 use std::fs;
-use std::io::*;
+// use std::os::*;
 // use tempdir::TempDir as TempDir;
 
 macro_rules! iotry {
@@ -31,91 +31,86 @@ macro_rules! iotry {
 }
 
 fn usage() {
-  println!("Usage: self-encrypt or decrypt a local file");
-  println!("       se -e <source_file>");
-  println!("       se -d <source_file>");
-  println!("       note: use absolute paths");
+  println!("Usage: self-encrypt or decrypt local file");
+  println!("       se [-e|-d] <full_file_name>");
+  println!("       note: the file name should be given as absolute path");
 }
 
-pub struct MyStorage<'b> {
-  source_path : &'b path::Path,
-  name : &'b str,
-  dir : &'b path::Path,
-  datamap_path : &'b path::Path
-}
+// pub struct MyStorage<'b> {
+//   dir : &'b path::Path
+// }
 
 
-impl<'b> MyStorage<'b> {
-  pub fn new(&self, filepath_ : &str) -> MyStorage<'b> {
-    MyStorage { source_path : match &path::Path::new(filepath_) {
-    			  Ok(x) => x,
-    			  Err(e) => panic!("Could not parse source file. {}", e)
-                },
-    			name : match self.source_path.file_name().unwrap(){
-    			  Ok(x) => x,
-    			  Err(e) => panic!("Source file is not a file. {}", e)
-    			},
-    			dir : match self.source_path.parent()
-    							   .unwrap()
-    							   .join(path::Path::new("chunks_".to_string()
-    							   	                     + &self.name)) {
-    			  Ok(x) => x,
-    			  Err(e) => panic!("Folder error.")
-    			},
-    			datamap_path : match self.dir.join("datamap") {
-    			  Ok(x) => x,
-    			  Err(e) => panic!("Couldn't create path to datamap.")
-    			}
-              }
-  }
-}
+// impl<'b> MyStorage<'b> {
+//   pub fn new(&self, filepath_ : &str) -> MyStorage<'b> {
+//     MyStorage { source_path : match path::Path::new(filepath_) {
+//     			  Ok(x) => x,
+//     			  Err(e) => panic!("Could not parse source file. {}", e)
+//                 },
+//     			name : match self.source_path.file_name().unwrap(){
+//     			  Ok(x) => x,
+//     			  Err(e) => panic!("Source file is not a file. {}", e)
+//     			},
+//     			dir : match self.source_path.parent()
+//     							   .unwrap()
+//     							   .join(path::Path::new("chunks_".to_string()
+//     							   	                     + &self.name)) {
+//     			  Ok(x) => x,
+//     			  Err(e) => panic!("Folder error.")
+//     			},
+//     			datamap_path : match self.dir.join("datamap") {
+//     			  Ok(x) => x,
+//     			  Err(e) => panic!("Couldn't create path to datamap.")
+//     			}
+//               }
+//   }
+// }
 
-impl<'b> self_encryption::Storage for MyStorage<'b> {
-  fn get(&self, name: Vec<u8>) -> Vec<u8> {
-    let file_name = String::from_utf8(name).unwrap();
-    let file_path = self.temp_dir.path().join(path::Path::new(&file_name)); 
-    let mut f = match fs::File::open(&file_path) {
-        // The `desc` field of `IoError` is a string that describes the error
-        Err(why) => panic!("on get couldn't open: {}", why),
-        Ok(file) => file,
-    };
-    let mut s = String::new();
-    //f.read_to_string(&mut s); put f into a string
-    match f.read_to_string(&mut s){
-        Err(why) => panic!("on get couldn't read: {}", why),
-        Ok(_) => print!("contains:\n{}", s),
-    }
-    s.into_bytes()
-  }
+// impl<'b> self_encryption::Storage for MyStorage<'b> {
+//   fn get(&self, name: Vec<u8>) -> Vec<u8> {
+//     let file_name = String::from_utf8(name).unwrap();
+//     let file_path = self.temp_dir.path().join(path::Path::new(&file_name)); 
+//     let mut f = match fs::File::open(&file_path) {
+//         // The `desc` field of `IoError` is a string that describes the error
+//         Err(why) => panic!("on get couldn't open: {}", why),
+//         Ok(file) => file,
+//     };
+//     let mut s = String::new();
+//     //f.read_to_string(&mut s); put f into a string
+//     match f.read_to_string(&mut s){
+//         Err(why) => panic!("on get couldn't read: {}", why),
+//         Ok(_) => print!("contains:\n{}", s),
+//     }
+//     s.into_bytes()
+//   }
 
-  #[allow(unused_must_use)]
-  fn put(&mut self, name: Vec<u8>, data: Vec<u8>) {
-    let file_name = String::from_utf8(name).unwrap();
-    let file_path = self.temp_dir.path().join(path::Path::new(&file_name)); 
-    let mut f = match fs::File::create(&file_path) {
-        // The `desc` field of `IoError` is a string that describes the error
-        Err(why) => panic!("on put couldn't open: {}", why),
-        Ok(file) => file,
-    };
-    f.write_all(&data);
-  }
-}
+//   #[allow(unused_must_use)]
+//   fn put(&mut self, name: Vec<u8>, data: Vec<u8>) {
+//     let file_name = String::from_utf8(name).unwrap();
+//     let file_path = self.temp_dir.path().join(path::Path::new(&file_name)); 
+//     let mut f = match fs::File::create(&file_path) {
+//         // The `desc` field of `IoError` is a string that describes the error
+//         Err(why) => panic!("on put couldn't open: {}", why),
+//         Ok(file) => file,
+//     };
+//     f.write_all(&data);
+//   }
+// }
 
 fn main() {
 
   use std::str::FromStr;
 
-  let args = std::os::args();
+  let args : Vec<String> = std::env::args().collect();
   let mut args = args.iter().map(|arg| &arg[..]);
-  let source : path::Path;
+  
+  // Skip program name
+  args.next();
 
   enum Mode {
 	Encrypt,
 	Decrypt
-  }
-
-  // Skip program name
-  args.next();
+  };
 
   let mode = match args.next() {
 	Some("-e") => Mode::Encrypt,
@@ -123,10 +118,13 @@ fn main() {
 	_ => { usage(); return; }
   };
 
-  let source = match args.next() {
-  	Some(x) => try!(FromStr::from_str(source)),
+  let source_str = match args.next() {
+  	Some(x) => x,
   	_ => { usage(); return; }
   };
+
+  let source = path::Path::new(source_str);
+
 
  //  let storage = match &args.collect::<Vec<_>>()[..] {
 	// [source, destination] => {
