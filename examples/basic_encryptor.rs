424a823ee06d471a706befb32d42342b044fb27c
David Irvine <david.irvine@maidsafe.net>
2015-04-07 21:42:58 +0100

    WIP sketch of very simple example use of self_encryptor (single file only) disk based chunkstore

---
+++ examples/basic_encryptor.rs
@@ -0,0 +1,120 @@
+// Copyright 2015 MaidSafe.net limited
+//
+// This MaidSafe Software is licensed to you under (1) the MaidSafe.net Commercial License,
+// version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+// licence you accepted on initial access to the Software (the "Licences").
+//
+// By contributing code to the MaidSafe Software, or to this project generally, you agree to be
+// bound by the terms of the MaidSafe Contributor Agreement, version 1.0, found in the root
+// directory of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also
+// available at: http://www.maidsafe.net/licenses
+//
+// Unless required by applicable law or agreed to in writing, the MaidSafe Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, either express or implied.
+//
+// See the Licences for the specific language governing permissions and limitations relating to
+// use of the MaidSafe Software.
+
+extern crate self_encryption;
+extern crate rand;
+extern crate tempdir;
+extern crate docopt;
+extern crate rustc_serialize;
+
+use docopt::Docopt;
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+use self_encryption::*;
+use std::string::String;
+use std::error::Error;
+
+
+// Write the Docopt usage string.
+static USAGE: &'static str = "
+Usage: basic_encryptor -e filename
+       basic_encryptor -d datamap destination
+       basic_encryptor -h | --help
+
+Options:
+    -h, --help      This message.
+    -e, --encrypt   encrypt a file.
+    -d, --decrypt   decrypt a file,
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    arg_filename: String,
+    arg_destination: String,
+    arg_datamap: String,
+    flag_encrypt: bool,
+    flag_decrypt: bool,
+}
+
+
+struct MyStorage;
+
+impl Storage for MyStorage {
+  fn get(&self, name: Vec<u8>) -> Vec<u8> {
+    let pathstr = match String::from_utf8(name) {
+      Err(_) => panic!("couldn't open file"),
+        Ok(file) => file,
+    };
+    let tmpname = ("chunk_store_test/".to_string() + &pathstr);
+    let path = Path::new(&tmpname);
+    let display = path.display();
+    let mut file = match File::open(&path) {
+      Err(_) => panic!("couldn't open {}", display),
+        Ok(f) => f,
+    };
+    let mut data = Vec::new();
+    file.read_to_end(&mut data);
+    data
+  }
+
+    fn put(&mut self, name: Vec<u8>, data: Vec<u8>) {
+    let pathstr = match String::from_utf8(name) {
+      Err(_) => panic!("couldn't open file"),
+        Ok(file) =>  file
+    }; 
+    let tmpname = ("chunk_store_test/".to_string() + &pathstr);
+    let path = Path::new(&tmpname);
+    let mut file = match File::create(&path) {
+           Err(_) => panic!("couldn't create"),
+           Ok(f) => f 
+    }; 
+           
+    match file.write_all(&data[..]) {
+             Err(_) => panic!("couldn't write "),
+             Ok(_) => println!("chunk  written")
+        };
+    }
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+
+    println!("{:?}", args);
+
+
+    match fs::create_dir(&Path::new("chunk_store_test")) {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(_) => {},
+    }
+    let mut my_storage = MyStorage;
+    let mut se = SelfEncryptor::new(&mut my_storage, datamap::DataMap::None);
+    
+   // se.write(&original, 0);
+    
+    
+   // let decrypted = se.read(read_position as u64, read_size as u64);
+
+
+
+    se.close();
+}
