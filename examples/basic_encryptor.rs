d7045ccdf078d37c7e36e05b738c96f30b8dd7e0
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-01-21 01:12:06 +0000

    Cleaned up example.

--- examples/basic_encryptor.rs
+++ examples/basic_encryptor.rs
@@ -15,49 +15,59 @@
 // Please review the Licences for the specific language governing permissions and limitations
 // relating to use of the SAFE Network Software.
 
+//! Basic example usage of a SelfEncryptor.
 
-extern crate self_encryption;
-extern crate rand;
-extern crate tempdir;
+// For explanation of lint checks, run `rustc -W help` or see
+// https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
+#![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
+          unknown_crate_types, warnings)]
+#![deny(deprecated, drop_with_repr_extern, improper_ctypes, missing_docs,
+        non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
+        private_no_mangle_fns, private_no_mangle_statics, stable_features, unconditional_recursion,
+        unknown_lints, unsafe_code, unused, unused_allocation, unused_attributes,
+        unused_comparisons, unused_features, unused_parens, while_true)]
+#![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
+        unused_qualifications, unused_results)]
+#![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
+         missing_debug_implementations, variant_size_differences)]
+
 extern crate docopt;
+#[macro_use]
+extern crate maidsafe_utilities;
 extern crate rustc_serialize;
-extern crate cbor;
+extern crate self_encryption;
 
-use std::fmt;
-use std::fs;
-use std::fs::File;
+use std::{env, fmt};
+use std::error::Error;
+use std::fs::{self, File};
 use std::io::prelude::*;
 use std::path::Path;
 use std::string::String;
-use std::error::Error;
 use std::sync::Arc;
 
 use docopt::Docopt;
-use cbor::{Decoder, Encoder};
-
-use self_encryption::*;
+use maidsafe_utilities::serialisation;
+use self_encryption::{DataMap, SelfEncryptor, Storage};
 
 // basic_encryptor -e filename
 // basic_encryptor -d datamap destination
 // basic_encryptor -h | --help
+#[rustfmt_skip]
 static USAGE: &'static str = "
 Usage: basic_encryptor -h
        basic_encryptor -e <target>
-       \
-                              basic_encryptor -d <target> <dest>
+       basic_encryptor -d <destination>
 
 Options:
-    -h, --help      \
-                              This message.
-    -e, --encrypt   encrypt a file.
-    -d, --decrypt   \
-                              decrypt a file,
+    -h, --help      Display this message.
+    -e, --encrypt   Encrypt a file.
+    -d, --decrypt   Decrypt a file.
 ";
 
 #[derive(RustcDecodable, Debug)]
 struct Args {
     arg_target: Option<String>,
-    arg_dest: Option<String>,
+    arg_destination: Option<String>,
     flag_encrypt: bool,
     flag_decrypt: bool,
     flag_help: bool,
@@ -82,7 +92,7 @@ fn file_name(name: &Vec<u8>) -> String {
     string
 }
 
-pub struct MyStorage {
+struct MyStorage {
     pub storage_path: String,
 }
 
@@ -93,11 +103,11 @@ impl Storage for MyStorage {
         let path = Path::new(&tmpname);
         let display = path.display();
         let mut file = match File::open(&path) {
-            Err(_) => panic!("couldn't open {}", display),
+            Err(error) => panic!("Failed to open chunk at {} - {:?}", display, error),
             Ok(f) => f,
         };
         let mut data = Vec::new();
-        file.read_to_end(&mut data).unwrap();
+        let _ = unwrap_result!(file.read_to_end(&mut data));
         data
     }
 
@@ -106,37 +116,40 @@ impl Storage for MyStorage {
         let tmpname = self.storage_path.clone() + &pathstr;
         let path = Path::new(&tmpname);
         let mut file = match File::create(&path) {
-            Err(_) => panic!("couldn't create"),
+            Err(error) => panic!("Failed to create {:?} - {:?}", path, error),
             Ok(f) => f,
         };
 
         match file.write_all(&data[..]) {
-            Err(_) => panic!("chunk write failed"),
-            Ok(_) => println!("chunk written"),
+            Err(error) => panic!("Failed to write chunk at {:?} - {:?}", path, error),
+            Ok(_) => println!("Chunk written to {:?}", path),
         };
     }
 }
 
 fn main() {
     let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| d.decode())
-                            .unwrap_or_else(|e| e.exit());
+                         .and_then(|d| d.decode())
+                         .unwrap_or_else(|e| e.exit());
     if args.flag_help {
         println!("{:?}", args)
     }
 
-    match fs::create_dir(&Path::new("chunk_store_test")) {
-        Err(why) => println!("!chunk_store_test {:?}", why.kind()),
-        Ok(_) => {}
-    }
-    let my_storage = Arc::new(MyStorage { storage_path: "chunk_store_test/".to_string() });
+    let mut chunk_store_dir = env::temp_dir();
+    chunk_store_dir.push("chunk_store_test/");
+    let _ = fs::create_dir(chunk_store_dir.clone());
+    let my_storage = Arc::new(MyStorage {
+        storage_path: unwrap_option!(chunk_store_dir.to_str(), "").to_owned(),
+    });
+    let mut data_map_file = chunk_store_dir;
+    data_map_file.push("data_map");
 
     if args.flag_encrypt && args.arg_target.is_some() {
-        if let Ok(mut file) = File::open(&args.arg_target.clone().unwrap()) {
+        if let Ok(mut file) = File::open(unwrap_option!(args.arg_target.clone(), "")) {
             match file.metadata() {
                 Ok(metadata) => {
                     if metadata.len() > self_encryption::MAX_MEMORY_MAP_SIZE as u64 {
-                        return println!("file size too large {} is greater than 1Gb",
+                        return println!("File size too large {} is greater than 1GB",
                                         metadata.len());
                     }
                 }
@@ -149,49 +162,61 @@ fn main() {
                 Err(error) => return println!("{}", error.description().to_string()),
             }
 
-            let mut se = SelfEncryptor::new(my_storage.clone(), datamap::DataMap::None);
+            let mut se = SelfEncryptor::new(my_storage.clone(), DataMap::None);
             se.write(&data, 0);
             let data_map = se.close();
 
-            if let Ok(mut file) = File::create("data_map") {
-                let mut encoded = Encoder::from_memory();
-                encoded.encode(&[&data_map]).unwrap();
-                if let Ok(_) = file.write_all(&encoded.as_bytes()[..]) {
-                    println!("data map written")
-                } else {
-                    return println!("data map write failed");
+            match File::create(data_map_file.clone()) {
+                Ok(mut file) => {
+                    let encoded = unwrap_result!(serialisation::serialise(&data_map));
+                    match file.write_all(&encoded[..]) {
+                        Ok(_) => println!("Data map written to {:?}", data_map_file),
+                        Err(error) => {
+                            println!("Failed to write data map to {:?} - {:?}",
+                                     data_map_file,
+                                     error);
+                        }
+                    }
                 }
-            } else {
-                return println!("failed to create data_map");
+                Err(error) => {
+                    println!("Failed to create data map at {:?} - {:?}",
+                             data_map_file,
+                             error);
+                }
             }
         } else {
-            return println!("failed to open {}", args.arg_target.clone().unwrap());
+            println!("Failed to open {}",
+                     unwrap_option!(args.arg_target.clone(), ""));
         }
     }
 
-    if args.flag_decrypt && args.arg_target.is_some() && args.arg_dest.is_some() {
-        if let Ok(mut file) = File::open(&args.arg_target.clone().unwrap()) {
+    if args.flag_decrypt && args.arg_destination.is_some() {
+        if let Ok(mut file) = File::open(data_map_file.clone()) {
             let mut data = Vec::new();
-            file.read_to_end(&mut data).unwrap();
-            let mut d = Decoder::from_bytes(data);
+            let _ = unwrap_result!(file.read_to_end(&mut data));
 
-            if let Ok(data_map) = d.decode().next().unwrap() {
+            if let Ok(data_map) = serialisation::deserialise::<DataMap>(&data) {
                 let mut se = SelfEncryptor::new(my_storage.clone(), data_map);
                 let length = se.len();
-                if let Ok(mut file) = File::create(&args.arg_dest.clone().unwrap()) {
+                if let Ok(mut file) = File::create(unwrap_option!(args.arg_destination.clone(),
+                                                                  "")) {
                     let content = se.read(0, length);
                     match file.write_all(&content[..]) {
-                        Err(_) => println!("file write failed"),
-                        Ok(_) => println!("file written"),
+                        Err(error) => println!("File write failed - {:?}", error),
+                        Ok(_) => {
+                            println!("File decrypted to {:?}",
+                                     unwrap_option!(args.arg_destination.clone(), ""))
+                        }
                     };
                 } else {
-                    return println!("failed to create {}", args.arg_dest.clone().unwrap());
+                    println!("Failed to create {}",
+                             unwrap_option!(args.arg_destination.clone(), ""));
                 }
             } else {
-                return println!("failed to decode data map possible corruption");
+                println!("Failed to parse data map - possible corruption");
             }
         } else {
-            return println!("failed to open {}", args.arg_target.clone().unwrap());
+            println!("Failed to open data map at {:?}", data_map_file);
         }
     }
 }
