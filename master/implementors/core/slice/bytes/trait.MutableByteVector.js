a3b0da41ed3e3ca2dbe97b32de93e688382a65e8
MaidSafe-QA <qa@maidsafe.net>
2016-01-22 03:57:49 +0000

    Updated documentation.

--- master/implementors/core/slice/bytes/trait.MutableByteVector.js
+++ master/implementors/core/slice/bytes/trait.MutableByteVector.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];
+implementors['libc'] = [];implementors['regex_syntax'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
