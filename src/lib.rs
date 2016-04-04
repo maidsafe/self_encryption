9b57d53c92a50ee2d67197e8cc5d32c80bd56e7c
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-04-04 10:14:07 +0100

    Trivial fix for failing test which was exceeding limits for serialising data via the utilities helpers.

--- src/lib.rs
+++ src/lib.rs
@@ -1705,8 +1705,7 @@ mod test {
 
     #[test]
     fn serialised_vectors() {
-        for vec_len in vec![1000, 2000, 5000, 10_000, 20_000, 50_000, 100_000, 20_0000, 50_0000,
-                            1_000_000] {
+        for vec_len in vec![1000, 2000, 5000, 10_000, 20_000, 50_000, 100_000, 200_000] {
             let storage = Arc::new(SimpleStorage::new());
             let datamap: DataMap = create_vector_data_map(storage.clone(), vec_len);
             check_vector_data_map(storage.clone(), vec_len, &datamap);
