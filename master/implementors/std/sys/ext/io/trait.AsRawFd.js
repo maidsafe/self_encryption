8b0bc65cd37955173394faf490ed7d6baf1a228b
MaidSafe-QA <qa@maidsafe.net>
2016-04-13 16:15:09 +0000

    Updated documentation.

---
+++ master/implementors/std/sys/ext/io/trait.AsRawFd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['net2'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/sys/ext/io/trait.AsRawFd.html' title='std::sys::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='net2/struct.TcpBuilder.html' title='net2::TcpBuilder'>TcpBuilder</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/sys/ext/io/trait.AsRawFd.html' title='std::sys::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='net2/struct.UdpBuilder.html' title='net2::UdpBuilder'>UdpBuilder</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
