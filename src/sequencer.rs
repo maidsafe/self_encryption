833b3a8d500f888918aff3df78d127f43ecee0e2
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2019-07-26 10:07:56 +0100

    chore/sequencer: update memmap and so avoid unsafe code

--- src/sequencer.rs
+++ src/sequencer.rs
@@ -7,17 +7,18 @@
 // permissions and limitations relating to use of the SAFE Network Software.
 
 use super::MAX_FILE_SIZE;
-use memmap::{Mmap, Protection};
+use memmap::MmapMut;
 use std::{
     io::{Error as IoError, Write},
     ops::{Deref, DerefMut, Index, IndexMut},
 };
+use unwrap::unwrap;
 
 pub const MAX_IN_MEMORY_SIZE: usize = 50 * 1024 * 1024;
 
 enum Data {
     Vector(Vec<u8>),
-    Mmap(Mmap),
+    Mmap(MmapMut),
 }
 
 /// Optionally create a sequence of bytes via a vector or memory map.
@@ -37,7 +38,7 @@ impl Sequencer {
     /// Initialise as a memory map
     pub fn new_as_mmap() -> Result<Sequencer, IoError> {
         Ok(Sequencer {
-            data: Data::Mmap(Mmap::anonymous(MAX_FILE_SIZE, Protection::ReadWrite)?),
+            data: Data::Mmap(MmapMut::map_anon(MAX_FILE_SIZE)?),
         })
     }
 
@@ -49,13 +50,12 @@ impl Sequencer {
         }
     }
 
-    #[allow(unsafe_code)]
     /// Initialise with the Sequencer with 'content'.
     pub fn init(&mut self, content: &[u8]) {
         match self.data {
             Data::Vector(ref mut vector) => vector.extend_from_slice(content),
             Data::Mmap(ref mut mmap) => {
-                let _ = unsafe { mmap.as_mut_slice() }.write_all(&content[..]);
+                let _ = (&mut mmap[..]).write_all(&content[..]);
             }
         }
     }
@@ -68,14 +68,13 @@ impl Sequencer {
         }
     }
 
-    #[allow(unsafe_code)]
     /// Create a memory map if we haven't already done so.
     pub fn create_mapping(&mut self) -> Result<(), IoError> {
         self.data = match self.data {
             Data::Mmap(_) => return Ok(()),
-            Data::Vector(ref mut vector) => {
-                let mut mmap = Mmap::anonymous(MAX_FILE_SIZE, Protection::ReadWrite)?;
-                let _ = unsafe { mmap.as_mut_slice() }.write_all(&vector[..]);
+            Data::Vector(ref vector) => {
+                let mut mmap = MmapMut::map_anon(MAX_FILE_SIZE)?;
+                (&mut mmap[..]).write_all(&vector[..])?;
                 Data::Mmap(mmap)
             }
         };
@@ -83,44 +82,40 @@ impl Sequencer {
     }
 }
 
-#[allow(unsafe_code)]
 impl Index<usize> for Sequencer {
     type Output = u8;
     fn index(&self, index: usize) -> &u8 {
         match self.data {
             Data::Vector(ref vector) => &vector[index],
-            Data::Mmap(ref mmap) => unsafe { &mmap.as_slice()[index] },
+            Data::Mmap(ref mmap) => unwrap!(mmap.get(index)),
         }
     }
 }
 
-#[allow(unsafe_code)]
 impl IndexMut<usize> for Sequencer {
     fn index_mut(&mut self, index: usize) -> &mut u8 {
         match self.data {
             Data::Vector(ref mut vector) => &mut vector[index],
-            Data::Mmap(ref mut mmap) => unsafe { &mut mmap.as_mut_slice()[index] },
+            Data::Mmap(ref mut mmap) => unwrap!(mmap.get_mut(index)),
         }
     }
 }
 
-#[allow(unsafe_code)]
 impl Deref for Sequencer {
     type Target = [u8];
     fn deref(&self) -> &[u8] {
         match self.data {
             Data::Vector(ref vector) => &*vector,
-            Data::Mmap(ref mmap) => unsafe { mmap.as_slice() },
+            Data::Mmap(ref mmap) => &*mmap,
         }
     }
 }
 
-#[allow(unsafe_code)]
 impl DerefMut for Sequencer {
     fn deref_mut(&mut self) -> &mut [u8] {
         match self.data {
             Data::Vector(ref mut vector) => &mut *vector,
-            Data::Mmap(ref mut mmap) => unsafe { &mut *mmap.as_mut_slice() },
+            Data::Mmap(ref mut mmap) => &mut *mmap,
         }
     }
 }
