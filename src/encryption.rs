ebdc0ab1a6a0df77d050b29d6426cbcb05807dcc
Yoga07 <yogeshwar_1997@hotmail.com>
2019-11-27 15:37:32 +0530

    fixup! use encrypt_vec/decrypt_vec methods to circumvent padding

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 MaidSafe.net limited.
+// Copyright 2019 MaidSafe.net limited.
 //
 // This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
 // Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
@@ -6,10 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-// TODO(dirvine) Look at aessafe 256X8 cbc it should be very much faster  :01/03/2015
-
 use crate::sequential::{Iv, Key};
-use crate::MAX_CHUNK_SIZE;
 use crate::{SelfEncryptionError, StorageError};
 use aes::Aes128;
 use block_modes::block_padding::Pkcs7;
@@ -19,21 +16,13 @@ type Aes128Cbc = Cbc<Aes128, Pkcs7>;
 pub const KEY_SIZE: usize = 16;
 pub const IV_SIZE: usize = 16;
 
-// Buffer size is set to max chunk size + 100 bytes for padding.
-pub const BUFFER_SIZE: usize = MAX_CHUNK_SIZE as usize + 100;
-
 pub fn encrypt<E>(data: &[u8], key: &Key, iv: &Iv) -> Result<Vec<u8>, SelfEncryptionError<E>>
 where
     E: StorageError,
 {
-    let cipher = Aes128Cbc::new_var(key.0.as_ref(), iv.0.as_ref()).unwrap();
-    let pos = data.len();
-    let mut buffer = [0u8; BUFFER_SIZE];
-    buffer[..pos].copy_from_slice(data);
-    cipher
-        .encrypt(&mut buffer, pos)
-        .map(|res| res.to_vec())
-        .map_err(|_| SelfEncryptionError::Encryption)
+    let cipher = Aes128Cbc::new_var(key.0.as_ref(), iv.0.as_ref())
+        .map_err(|e| SelfEncryptionError::Cipher(format!("{:?}", e)))?;
+    Ok(cipher.encrypt_vec(data))
 }
 
 pub fn decrypt<E>(
@@ -44,10 +33,9 @@ pub fn decrypt<E>(
 where
     E: StorageError,
 {
-    let cipher = Aes128Cbc::new_var(key.0.as_ref(), iv.0.as_ref()).unwrap();
-    let mut buffer = encrypted_data.to_vec();
+    let cipher = Aes128Cbc::new_var(key.0.as_ref(), iv.0.as_ref())
+        .map_err(|err| SelfEncryptionError::Cipher(format!("{:?}", err)))?;
     cipher
-        .decrypt(&mut buffer)
-        .map(|res| res.to_vec())
-        .map_err(|_| SelfEncryptionError::Decryption)
+        .decrypt_vec(encrypted_data)
+        .map_err(|err| SelfEncryptionError::Decryption(format!("{:?}", err)))
 }
