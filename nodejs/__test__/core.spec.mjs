f9fb00fb6cbe9b34356c1bea8a7827a9cfc171ac
Benno Zeeman <bzeeman@live.nl>
2025-05-12 09:10:29 +0200

    test: add verifyChunk test for Node.js

--- nodejs/__test__/core.spec.mjs
+++ nodejs/__test__/core.spec.mjs
@@ -1,10 +1,22 @@
 import test from 'ava'
 
-import { encrypt, decrypt } from '../index.js'
+import { encrypt, decrypt, verifyChunk } from '../index.js'
 
 test('encrypt and decrypt', async (t) => {
     const data = Buffer.from("Hello, World!");
     const { dataMap, chunks } = encrypt(data)
     const dataDecrypted = decrypt(dataMap, chunks)
     t.deepEqual(Buffer.from(dataDecrypted), data)
+})
+
+
+test('verify chunk', async (t) => {
+    t.plan(0) // Expect no assertions in this test
+
+    const data = Buffer.from("Hello, World!");
+    const { dataMap, chunks } = encrypt(data)
+    const infos = dataMap.infos()
+    for (const [i, info] of infos.entries()) {
+        verifyChunk(info.dstHash, chunks[i].content())
+    }
 })
